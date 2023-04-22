6da4632a171d7c1a0815893544dbf54c273f065d
Vinicius <viniciuscastrogs@gmail.com>
2023-04-22 20:37:30 -0300

    projeto criado

---
+++ src/test/java/com/vinicgs/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinicgs.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
