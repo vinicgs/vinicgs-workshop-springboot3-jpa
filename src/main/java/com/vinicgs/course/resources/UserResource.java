84df88e74e6df49d34a901c48828d48686ffde72
Vinicius <viniciuscastrogs@gmail.com>
2023-04-23 15:27:44 -0300

    H2 database, test profile, JPA

--- src/main/java/com/vinicgs/course/resources/UserResource.java
+++ src/main/java/com/vinicgs/course/resources/UserResource.java
@@ -1,4 +1,4 @@
-package com.vinicgs.course.resource;
+package com.vinicgs.course.resources;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +13,6 @@ public class UserResource {
 
 	@GetMapping
 	public ResponseEntity<User> findAll() {
-
 		User user = new User(1L, "vinicius", "vinicgs@gmail.com", "99999999", "513");
 		return ResponseEntity.ok().body(user);
 	}
