d0465a3f8c2390fa143e0063914ec85fb58acb1e
Vinicius <viniciuscastrogs@gmail.com>
2023-04-22 21:44:13 -0300

    User entity end resource

---
+++ src/main/java/com/vinicgs/course/resource/UserResource.java
@@ -0,0 +1,21 @@
+package com.vinicgs.course.resource;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vinicgs.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+
+		User user = new User(1L, "vinicius", "vinicgs@gmail.com", "99999999", "513");
+		return ResponseEntity.ok().body(user);
+	}
+
+}
